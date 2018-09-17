49cc37d64d0ca19ac2e91260a8a72cae1f24f297
Doug Gschwind <doug.gschwind@gmail.com>
2018-09-16 19:14:31 -0700

    Rough in POST /users endpoint

---
+++ spring-boot-security-jpa/src/main/java/com/douggschwind/examples/resources/AbstractResource.java
@@ -0,0 +1,17 @@
+package com.douggschwind.examples.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public abstract class AbstractResource {
+
+    protected final ResponseEntity<URI> generateCreatedResponse(String uriOfNewlyCreatedResource) {
+        return ResponseEntity.created(URI.create(uriOfNewlyCreatedResource)).build();
+    }
+
+}
