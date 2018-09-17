11a3a71625f085c39f246d2cf86a40c3b9f36d35
Doug Gschwind <doug.gschwind@gmail.com>
2018-09-16 19:19:03 -0700

    Rough in POST /users endpoint

--- spring-boot-security-jpa/src/main/java/com/douggschwind/examples/resources/UsersResource.java
+++ spring-boot-security-jpa/src/main/java/com/douggschwind/examples/resources/UsersResource.java
@@ -3,7 +3,6 @@ package com.douggschwind.examples.resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
